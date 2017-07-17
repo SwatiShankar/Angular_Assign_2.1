a6f11aeedb31c7f7b4088711a6c02b867e7ec9ac
Swati shankar <swati.shankar22@gmail.com>
2017-07-17 22:37:49 +0530

    Assignment_Assignment 2.1

---
+++ function.js
@@ -0,0 +1,6 @@
+// => indicates that the function below is a fat arrow function
+var fatArraowFn = function (name, age) {
+    return "My name is : " + name + " and I am " + age + " years old";
+};
+var fatArraow = fatArraowFn("Shruthi", 30);
+console.log(fatArraow);
---
+++ function.ts
@@ -0,0 +1,8 @@
+// => indicates that the function below is a fat arrow function
+let fatArraowFn = (name : String , age : number) : String => {
+	return "My name is : " + name +" and I am " + age + " years old";
+}
+
+let fatArraow= fatArraowFn("Shruthi",30);
+
+console.log(fatArraow);
